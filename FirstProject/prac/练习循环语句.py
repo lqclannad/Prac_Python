a83316f9e23123f96f3fda3c80671cd1f619f1c6
lqclannad <1021118746@qq.com>
2021-10-09 09:26:57 +0800

    "添加了九九乘法表的while循环语句"

--- FirstProject/prac/练习循环语句.py
+++ FirstProject/prac/练习循环语句.py
@@ -15,8 +15,18 @@
 #         print("* ", end="")
 #     print()
 
-# 九九乘法表
-for i in range(1, 10):
-    for j in range(1, i+1):
+# 九九乘法表 (for)
+# for i in range(1, 10):
+#     for j in range(1, i+1):
+#         print(f'{i}*{j}={i*j} ', end='')
+#     print()
+
+# 九九乘法表 (while)
+i = 1
+while i < 10:
+    j = 1
+    while j < i+1:
         print(f'{i}*{j}={i*j} ', end='')
+        j += 1
+    i += 1
     print()
