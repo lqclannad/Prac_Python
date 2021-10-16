6cc5e76e96fa4a8df8a69c21127a9dc2d0bfb5e6
lqclannad <1021118746@qq.com>
2021-10-17 00:00:04 +0800

    “json”

--- FirstProject/prac2/file_operation.py
+++ FirstProject/prac2/file_operation.py
@@ -7,7 +7,6 @@ import os.path
 
 # mode- r读 w写 w+读写 a追加
 # 读写不能同时进行
-'''
 filename = "test.txt"
 
 file = open(filename, "r")
@@ -15,10 +14,8 @@ file = open(filename, "r")
 str = file.read()
 print(str)
 file.close()
-'''
 
 # 判断文件夹是否存在，不存在就创建
-'''
 path = r"G:\code\python\Prac_Python\FirstProject\prac2\test.txt"
 file_path, file_name = os.path.split(path)
 if not os.path.exists(file_path):
@@ -27,16 +24,15 @@ if not os.path.exists(file_path):
 file = open(path, "w")
 file.write("Python 是一门很好的语言！")
 file.close()
-'''
 
 # ★read() 返回一个包装好的字符内容
 # readline() 返回第一行内容
 # ★readlines() 返回一个列表，以行为元素
-path = r"G:\code\python\Prac_Python\FirstProject\prac2\test.txt"
+path = r"D:\Code\Pycharm\Prac_Python\FirstProject\prac2\test.txt"
 file = open(path, "r")
-str = file.read()
-str = file.readline()
-str = file.readlines()
+# str = file.read()
+# str = file.readline()
+# str = file.readlines()
 for line in file.readlines():
     print(line)
 # in后面跟的是一个序列，列表是序列，序列不一定是列表
---
+++ FirstProject/prac2/json.py
@@ -0,0 +1,7 @@
+# _*_ coding:utf-8 _*_
+# 我的名字: Administrator-LQ
+# 创建时间: 2021/10/16 23:58
+# 文件名称: json.py
+# 开发工具: Pycharm
+import json
+
