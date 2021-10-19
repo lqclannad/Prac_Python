0f7435487f7d831b917514acb304bc08a5b1a558
lqclannad <1021118746@qq.com>
2021-10-19 19:20:23 +0800

    "credit the and.py"

--- SecondProject/MathLesson/and.py
+++ SecondProject/MathLesson/and.py
@@ -8,17 +8,19 @@ import random
 xs = [[0, 0], [0, 1], [1, 0], [1, 1]]
 ys = [0, 0, 0, 1]
 
-gamma = 0.02
+gamma = 0.1
 
-ws = [random.random() for i in range(3)]
+ws = [random.random() for _ in range(3)]
 print(ws)
 
-for _ in range(400):
+for _ in range(500):
     for x,y in zip(xs, ys):
         _y = x[0] * ws[0] + x[1] * ws[1] + ws[2]
         e = _y - y
         # s = 2 * e * x[0]
-        ws[0] = ws[0] - 2 * e * x[0]
-        ws[1] = ws[1] - 2 * e * x[1]
-        ws[2] = ws[2] - 2 * e
+        ws[0] = ws[0] - gamma * e * x[0]
+        ws[1] = ws[1] - gamma * e * x[1]
+        ws[2] = ws[2] - gamma * e
         print(e ** 2)
+
+    print(-1 * ws[0] + 0 * ws[1] + ws[2])
